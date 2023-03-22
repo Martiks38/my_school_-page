6f681a1e0bc3a734ea6892e034033283506f15fe
Giuliano Cappa <cappa.giuliano@gmail.com>
2023-03-21 22:50:34 -0300

    Modify initial files

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,19 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang="es">
+			<Head>
+				<link rel="icon" href="/favicon.ico" />
+				<link rel="apple-touch-icon" href="/logo192.png" />
+				<link rel="apple-touch-icon" href="/logo310.png" />
+				<link rel="manifest" href="/manifest.json" />
+				<meta name="theme-color" content="#000000" />
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
 }
