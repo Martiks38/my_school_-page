548bf5cce04ab7a4673b8f1767f5bab99c506245
Martiks38 <cappa.giuliano@gmail.com>
2023-04-06 11:07:03 -0300

    Crear constante notices

--- src/const.js
+++ src/const.js
@@ -1,3 +1,126 @@
+export const notices = [
+	{
+		id: '1c3a',
+		title: 'Becas Provinciales 2023',
+		description:
+			'Actualmente se encuentra abierta la incripción a becas provinciales hasta el 13 de abril',
+		content: {
+			text: null,
+			list: {
+				id: 'c52a',
+				items: [
+					{ id: 1, item: 'Ser alumnos del establecimiento' },
+					{
+						id: 2,
+						item: 'Completar el formulario de becas disponible en la sección de alumnos o en la fotocopiadora del establecimiento'
+					},
+					{
+						id: 3,
+						item: 'Presentar la documentación requerida: –  Declaración jurada de grupo familiar conviviente, copia del recibo sueldo o Certificación negativa de Anses'
+					},
+					{
+						id: 4,
+						item: 'Tener ingresos inferiores a $163.000'
+					},
+					{
+						id: 5,
+						item: 'De tener CBU/CVU consignarlo claramente en el formulario'
+					},
+					{
+						id: 6,
+						item: 'Colocar el número celular y la empresa a la cual pertenece'
+					}
+				],
+				item_link: [2, 3],
+				links: [
+					'https://drive.google.com/file/d/1sio_1VPsCXfejnM8yKS3QBzD5zVNqZ0r/view?usp=sharing',
+					' https://drive.google.com/file/d/1-yd-YHfxk62SSkK_VsowyT_9yZK5CtaB/view?usp=sharing'
+				]
+			}
+		}
+	},
+	{
+		id: '1c3a',
+		title: 'Becas Provinciales 2023',
+		description:
+			'Actualmente se encuentra abierta la incripción a becas provinciales hasta el 13 de abril',
+		content: {
+			text: null,
+			list: {
+				id: 'c52a',
+				items: [
+					{ id: 1, item: 'Ser alumnos del establecimiento' },
+					{
+						id: 2,
+						item: 'Completar el formulario de becas disponible en la sección de alumnos o en la fotocopiadora del establecimiento'
+					},
+					{
+						id: 3,
+						item: 'Presentar la documentación requerida: –  Declaración jurada de grupo familiar conviviente, copia del recibo sueldo o Certificación negativa de Anses'
+					},
+					{
+						id: 4,
+						item: 'Tener ingresos inferiores a $163.000'
+					},
+					{
+						id: 5,
+						item: 'De tener CBU/CVU consignarlo claramente en el formulario'
+					},
+					{
+						id: 6,
+						item: 'Colocar el número celular y la empresa a la cual pertenece'
+					}
+				],
+				item_link: [2, 3],
+				links: [
+					'https://drive.google.com/file/d/1sio_1VPsCXfejnM8yKS3QBzD5zVNqZ0r/view?usp=sharing',
+					' https://drive.google.com/file/d/1-yd-YHfxk62SSkK_VsowyT_9yZK5CtaB/view?usp=sharing'
+				]
+			}
+		}
+	},
+	{
+		id: '1c3a',
+		title: 'Becas Provinciales 2023',
+		description:
+			'Actualmente se encuentra abierta la incripción a becas provinciales hasta el 13 de abril',
+		content: {
+			text: null,
+			list: {
+				id: 'c52a',
+				items: [
+					{ id: 1, item: 'Ser alumnos del establecimiento' },
+					{
+						id: 2,
+						item: 'Completar el formulario de becas disponible en la sección de alumnos o en la fotocopiadora del establecimiento'
+					},
+					{
+						id: 3,
+						item: 'Presentar la documentación requerida: –  Declaración jurada de grupo familiar conviviente, copia del recibo sueldo o Certificación negativa de Anses'
+					},
+					{
+						id: 4,
+						item: 'Tener ingresos inferiores a $163.000'
+					},
+					{
+						id: 5,
+						item: 'De tener CBU/CVU consignarlo claramente en el formulario'
+					},
+					{
+						id: 6,
+						item: 'Colocar el número celular y la empresa a la cual pertenece'
+					}
+				],
+				item_link: [2, 3],
+				links: [
+					'https://drive.google.com/file/d/1sio_1VPsCXfejnM8yKS3QBzD5zVNqZ0r/view?usp=sharing',
+					' https://drive.google.com/file/d/1-yd-YHfxk62SSkK_VsowyT_9yZK5CtaB/view?usp=sharing'
+				]
+			}
+		}
+	}
+]
+
 export const sections = [
 	{
 		id: 'abaf',
