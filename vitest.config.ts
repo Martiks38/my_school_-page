e9f8f42433fb77e46a48540522cd24f20fab6e56
Martiks38 <cappa.giuliano@gmail.com>
2023-04-07 15:18:52 -0300

    Untraked pnpm-lock.yaml

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,18 @@
+import path from 'node:path'
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
+const resolve = (route: string) => path.resolve(process.cwd(), route)
+
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    coverage: { provider: 'c8' }
-  }
+	plugins: [react()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		coverage: { provider: 'c8' },
+		alias: {
+			'@': resolve('./src'),
+			'@components': resolve('./src/components')
+		}
+	}
 })
