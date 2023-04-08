59cdd77ae594bde393a6cd149f6f416741d61ea3
Martiks38 <cappa.giuliano@gmail.com>
2023-04-08 14:02:27 -0300

    Hacer test de institucional/historia

---
+++ src/pages/institucional/historia.test.tsx
@@ -0,0 +1,18 @@
+import { describe, expect, it, vi } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import matchers from '@testing-library/jest-dom/matchers'
+import HistoryInstitution from './historia'
+
+expect.extend(matchers)
+
+describe('<HistoryInstitution />', () => {
+	render(<HistoryInstitution />)
+
+	it('It should render correctly', () => {
+		expect(
+			screen.getByAltText(
+				'Imagen conmemorativa de los 80 años de la Escuela de Enseñanza Técnica N°477'
+			)
+		).toBeInTheDocument()
+	})
+})
