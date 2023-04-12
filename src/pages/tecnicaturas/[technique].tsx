70a6d82c0957b3fdea64997f13851d17cd61fdb9
Martiks38 <cappa.giuliano@gmail.com>
2023-04-12 18:23:58 -0300

    Hacer responsive de la ruta [technique]

--- src/pages/tecnicaturas/[technique].tsx
+++ src/pages/tecnicaturas/[technique].tsx
@@ -5,7 +5,7 @@ import { LayoutPage } from '@/Layout/LayoutPage'
 import { ListItems } from '@/components/ListItem'
 import { TableSubjects } from '@/components/TableSubjects'
 import { dataTechniques } from '@/const'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 type TechniqueTypes = 'electronica' | 'electromecanica' | 'renovables'
 
@@ -21,19 +21,33 @@ type TechnicalSection = {
 	}[]
 }
 
+type Video = {
+	[index in 'electromecanica' | 'electronica' | 'renovables']: string
+}
+
 const title = {
 	electromecanica: 'Electromecánica',
 	electronica: 'Electrónica',
 	renovables: 'Energías renovables'
 }
 
+const videos: Video = {
+	renovables: 'https://www.youtube.com/embed/ZahRlFI6xa0',
+	electronica: 'https://drive.google.com/file/d/1t02K3nHXSdU-2V_7pYqPQDmMt_j0uigU/preview',
+	electromecanica: 'https://drive.google.com/file/d/1Y55LTJv_rnVo_HUsI7aHWNEc2hwm6aaL/preview'
+}
+
 export default function Techniques() {
-	const [viewVideo, setViewVideo] = useState(false)
+	const [src, setSrc] = useState('')
 	const router = useRouter()
 	const path = router.query.technique as TechniqueTypes
 	const technique = title[path]
 
 	const sections = dataTechniques[path as TechniqueTypes]?.sections
+
+	useEffect(() => {
+		setSrc(videos[path])
+	}, [path])
 
 	return (
 		<>
@@ -41,10 +55,10 @@ export default function Techniques() {
 				<title>{technique} | EET N°477</title>
 			</Head>
 			<LayoutPage>
-				<main className="w-full max-w-primaryContent mx-auto mt-8 p-4 xl:border border-[#0002]">
+				<main className="w-full max-w-primaryContent mx-auto mt-8 px-8 xl:px-4 py-4 xl:border border-[#0002]">
 					<h1 className="mb-8 text-4xl font-robotoMono font-bold">Tecnicatura en {technique}</h1>
 					<div className="w-fit">
-						<div className="w-[50rem] h-[28.125rem] mb-10">
+						<div className="max-w-[50rem] w-full max-h-[28.125rem] h-auto mb-10">
 							{path && (
 								<Image
 									src={path && `/images/${path}.webp`}
@@ -56,51 +70,15 @@ export default function Techniques() {
 								/>
 							)}
 						</div>
-						<div className="w-full border-l-8 border-[#0002]" aria-live="polite">
-							{!viewVideo ? (
-								<button
-									className="flex justify-center items-center w-[35rem] mx-auto aspect-video bg-[url('/Escudo.webp')] bg-no-repeat bg-contain bg-center border border-[#0008]"
-									onClick={() => setViewVideo(true)}
-									aria-label={`Ver trabajos realizados por los estudiantes de ${technique}`}
-								>
-									<div className="p-6 rounded-lg hover:bg-[#000b] drop-shadow-[0_0_6px_rgb(0,0,0)]">
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											width="800"
-											height="800"
-											viewBox="-0.5 0 7 7"
-											className="w-12 h-12"
-										>
-											<path
-												fill="#fff"
-												fillRule="evenodd"
-												d="M5.5 2.57 1.5.14C.83-.26 0 .25 0 1.07v4.86c0 .82.83 1.33 1.5.93l4-2.43a1.1 1.1 0 0 0 0-1.86"
-											/>
-										</svg>
-									</div>
-								</button>
-							) : path === 'renovables' ? (
-								<iframe
-									width="560"
-									height="315"
-									src="https://www.youtube.com/embed/ZahRlFI6xa0?autoplay=1"
-									title="YouTube video player"
-									allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-									allowFullScreen
-									className="w-[35rem] mx-auto aspect-video"
-									aria-label={`Video de los trabajos realizados por los alumnos de ${technique}`}
-								></iframe>
-							) : (
-								<video
-									src={path && `/videos/${path}.mp4`}
-									controls
-									controlsList="nodownload"
-									autoPlay
-									className="w-[35rem] mx-auto aspect-video"
-									aria-label={`Video de los trabajos realizados por los alumnos de ${technique}`}
-								></video>
-							)}
-						</div>
+						<iframe
+							width="560"
+							height="315"
+							src={src}
+							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+							allowFullScreen
+							className="max-w-[35rem] w-full h-auto mx-auto aspect-video"
+							title={`Video de los trabajos realizados por los alumnos de ${technique}`}
+						></iframe>
 					</div>
 					<div className="max-w-[50rem]">
 						<section className="my-10">
