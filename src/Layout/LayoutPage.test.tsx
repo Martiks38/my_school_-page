72febbf1b38fbabb2c4d9a089803c2e0ccdfba06
Martiks38 <cappa.giuliano@gmail.com>
2023-04-12 00:46:53 -0300

    Eliminar tests de mt-[60vh] de LayoutPage
    Se cambiaron los estilos de flex a grid dejando en desuso la clase

--- src/Layout/LayoutPage.test.tsx
+++ src/Layout/LayoutPage.test.tsx
@@ -28,19 +28,4 @@ describe('<LayoutPage />', () => {
 
 		expect(screen.getByText('Content')).toBeInTheDocument()
 	})
-
-	it('If the page does not have a hero image, the content wrapper does not have the mt-[60vh] class.', () => {
-		const { container } = renderComponent()
-
-		const WrapperContent = container.querySelector('.mt-auto > div')
-
-		expect(WrapperContent).not.toHaveClass('mt-[60vh]')
-	})
-
-	it('If haveHeroImage is true, the wrapper must have the class mt-[60vh].', () => {
-		const { container } = renderComponent(true)
-		const WrapperContent = container.querySelector('.mt-auto > div')
-
-		expect(WrapperContent).toHaveClass('mt-[60vh]')
-	})
 })
