cfff94a060d153e4434391e4dbda2895cccc0875
Martiks38 <cappa.giuliano@gmail.com>
2023-04-19 00:29:34 -0300

    Volver a configuración webpack 4

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	webpack5: true,
-	webpack: (config) => {
-		config.resolve.fallback = { fs: false }
+	webpack: (config, { isServer }) => {
+		// Fixes npm packages that depend on `fs` module
+		if (!isServer) {
+			config.node = {
+				fs: false
+			}
+		}
 
 		return config
 	}
